96986a9d94e62165164f8c066b34e20a784e0b5d
PensCup87 <bdt9123@gmail.com>
2017-11-06 13:34:20 -0500

    Initial Commit

---
+++ GitGroupTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitGroupTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (1 < 2)
+            {
+                Console.WriteLine("Math works");
+            }
+            else
+            {
+                Console.WriteLine("What are you thinking");
+            }
+        }
+    }
+}
